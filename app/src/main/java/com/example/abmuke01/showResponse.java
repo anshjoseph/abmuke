c41318a2cb012cf0e8d44e8bc3c36443306eab41
R Ansh Joseph <ranshjosrph@gmail.com>
2022-07-07 11:22:09 +0530

    adding ne upadtes

--- app/src/main/java/com/example/abmuke01/showResponse.java
+++ app/src/main/java/com/example/abmuke01/showResponse.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.abmuke01.API.AbmukeAPI;
@@ -27,6 +28,7 @@ public class showResponse extends AppCompatActivity {
     private String gettxt;
     private AbmukeAPI_Model response;
     private int count;
+    private TextView load;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +36,12 @@ public class showResponse extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         gettxt = extras.getString("string");
         Toast.makeText(this, gettxt, Toast.LENGTH_SHORT).show();
+        load = findViewById(R.id.output);
         AbmukeAPI.GetData(gettxt, new AsyncRun_onSuccess() {
             @Override
             public void run(Response response) {
                 AbmukeAPI_Model abmukemodel = (AbmukeAPI_Model) response.body();
+                String output[] = abmukemodel.getString().split(" ");
                 Toast.makeText(showResponse.this, abmukemodel.toString(), Toast.LENGTH_SHORT).show();
                 List<List<String>> res = abmukemodel.getRes();
 
@@ -55,6 +59,7 @@ public class showResponse extends AppCompatActivity {
 
 
                 // init img deploy
+                load.setText(output[count]);
                 if(res.get(count).get(res.get(count).size()-1).equals("0")) new CWebView(web).loadHtml(new BootstrapCodeBuilder(new video(res.get(count).get(0))));
                 if (res.get(count).get(res.get(count).size() - 1).equals("1")) {
                     List<String> temp  = res.get(count).subList(0,res.get(count).size()-1);
@@ -79,6 +84,7 @@ public class showResponse extends AppCompatActivity {
                                 Log.d("cas",""+temp);
                                 new CWebView(web).loadHtml(new BootstrapCodeBuilder(new carousel(temp)));
                             }
+                            load.setText(output[count]);
                         }else Toast.makeText(showResponse.this, "max", Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -94,6 +100,7 @@ public class showResponse extends AppCompatActivity {
                                 List<String> temp  = res.get(count).subList(0,res.get(count).size()-1);
                                 new CWebView(web).loadHtml(new BootstrapCodeBuilder(new carousel(temp)));
                             }
+                            load.setText(output[count]);
                         }else Toast.makeText(showResponse.this, "min", Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -103,6 +110,7 @@ public class showResponse extends AppCompatActivity {
             @Override
             public void run() {
                 Toast.makeText(showResponse.this, "some thing went wrong", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
